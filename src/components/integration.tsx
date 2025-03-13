f067da401b45d492f394bc645798044bc8cbc134
kunalsharma3012 <kunalsharma93103@gmail.com>
2025-03-13 14:33:37 +0530

    project completed

--- src/components/integration.tsx
+++ src/components/integration.tsx
@@ -1,27 +1,46 @@
+"use client";
+
 import Image from "next/image";
 import { type IntegrationsType } from "@/sections/Integrations";
 import { twMerge } from "tailwind-merge";
+import { motion } from "framer-motion";
+import { Fragment } from "react";
 
 export default function IntegrationColumn(props: {
     integrations: IntegrationsType;
     className?: string;
+    reverse?:boolean;
 }) {
-    const { integrations, className } = props;
+    const { integrations, className, reverse } = props;
 
     return (
-            <div className={twMerge("flex flex-col gap-4 pb-4",className
+            <motion.div
+            initial={{
+                y:reverse ? '-50%':0,
+            }}
+            animate={{
+                y: reverse ? 0 : "-50%",
+            }}
+            transition={{
+                duration:15,
+                repeat:Infinity,
+                ease:"linear"
+            }}
+             className={twMerge("flex flex-col gap-4 pb-4",className
             )}>
-                {integrations.map((integration) => (
-                    <div
+                {Array.from({ length: 2}).map((_,i)=>(
+                    <Fragment key={i}>
+                    {integrations.map((integration) => (
+                        <div
                         key={integration.name}
-                        className="bg-neutral-900 border-white/10 rounded-3xl p-6"
-                    >
+                        className="bg-neutral-900 border border-white/10 rounded-3xl p-6"
+                        >
                         <div className="flex justify-center">
                             <Image
                                 src={integration.icon}
                                 alt={integration.name}
                                 className="size-24"
-                            />
+                                />
                         </div>
                         <h3 className="text-3xl text-center mt-6">
                             {integration.name}
@@ -31,7 +50,9 @@ export default function IntegrationColumn(props: {
                         </p>
                     </div>
                 ))}
-            </div>
+                </Fragment>
+            ))}
+            </motion.div>
         
-    );
+    )
 }
--- src/sections/Integrations.tsx
+++ src/sections/Integrations.tsx
@@ -64,6 +64,7 @@ export default function Integrations() {
                         <IntegrationColumn
                             integrations={integrations.slice().reverse()}
                             className="hidden md:flex"
+                            reverse = {true}
                         />
                     </div>
                     </div>
